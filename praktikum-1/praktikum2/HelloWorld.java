c2373b3b79826e4a81d13509da7a6bf2f1d32596
Mutia Insyana <insyanamutia@gmail.com>
2026-09-12 12:42:51 +0700

    A.1. Struktur Dasar Program Java

---
+++ praktikum-1/praktikum2/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Mutia Insyana");
+
+            System.out.print("Nama:");
+            System.out.print("Mutia Insyana");
+            System.out.println();
+            System.out.println("Ini baris baru");
+
+            // Program menampilkan biodata sederhana
+            System.out.println("Nama: Mutia Insyana");
+            /* Baris di bawah ini
+            mencetak Nim mahasiswa
+             */
+            System.out.println("NIM: 3010020");
+        }
+    }
